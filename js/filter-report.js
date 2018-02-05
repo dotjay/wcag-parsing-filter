418b16b7ba3c50987cebc3f6692027fd6d3e77ae
Jon Gibbins <dotjay@dotjay.co.uk>
2018-02-05 10:57:13 +0000

    Version 2 - Reports the number of errors and warnings found affecting accessibility and describes each.

--- js/filter-report.js
+++ js/filter-report.js
@@ -3,11 +3,13 @@
  * Author: Jon Gibbins
  * Based on code by Steve Faulkner
  * https://developer.paciellogroup.com/blog/2012/02/wcag-2-0-parsing-error-bookmarklet/
- * Version 1
- * Reports total number of errors and the number related to accessibility. Faster code.
+ * Version 2
+ * Reports the number of errors and warnings found affecting accessibility and describes each.
  *
  * Bookmarklet
- * javascript:(function(){var filterStrings=["tag seen","Stray end tag","Bad start tag","violates nesting rules","Duplicate ID","first occurrence of ID","Unclosed element","not allowed as child of element","unclosed elements","not allowed on element","unquoted attribute value","Duplicate attribute"],filterRE=filterStrings.join("|"),i,nAcc=0,nErr=0,result,resultText,results,root=document.getElementById("results");if(!root){return;}results=root.getElementsByTagName("li");for(i=results.length-1;i>=0;i--){result=results[i];if(result.id.substr(0,3)==="vnu"){if(result.className!=="info"){nErr=nErr+1;}resultText=result.textContent+"";if(resultText.match(filterRE)==null){result.style.display="none";result.className=result.className+"steveNoLike";}else{nAcc=nAcc+1;}}}alert(nErr+" errors and warnings, "+nAcc+" related to accessibility");})();
+ * Compressed with Crunchinator:
+ * http://ted.mielczarek.org/code/mozilla/bookmarklet.html
+ * javascript:(function(){var filterStrings=["tag seen","Stray end tag","Bad start tag","violates nesting rules","Duplicate ID","first occurrence of ID","Unclosed element","not allowed as child of element","unclosed elements","not allowed on element","unquoted attribute value","Duplicate attribute"],filterRE=filterStrings.join("|"),i,nAcc=0,nErr=0,result,resultText,results,resultsErrors={},resultsWarnings={},root=document.getElementById("results");if(!root){return;}results=root.getElementsByTagName("li");for(i=results.length-1;i>=0;i--){result=results[i];if(result.id.substr(0,3)==="vnu"){if(result.className!=="info"){nErr=nErr+1;}resultText=""+result.textContent;resultText=resultText.substring(0,resultText.indexOf('.'));if(resultText.match(filterRE)==null){result.style.display="none";result.className=result.className+"steveNoLike";}else{nAcc=nAcc+1;if(resultText.substring(0,6)=='Error:'){resultsErrors[resultText.substr(7)]=true;}}if(resultText.substring(0,8)=='Warning:'){resultsWarnings[resultText.substr(9)]=true;}}}resultText="";for(i in resultsWarnings){if(resultsWarnings.hasOwnProperty(i)){resultText=i+"; "+resultText;}}resultText="\n\nWarnings: \n"+resultText;for(i in resultsErrors){if(resultsErrors.hasOwnProperty(i)){resultText=i+"; "+resultText;}}resultText=nErr+" errors and warnings.\n\n"+nAcc+" errors found that impact accessibility: "+resultText;alert(resultText);})();
  */
 javascript: (function() {
 	var filterStrings = ["tag seen", "Stray end tag", "Bad start tag", "violates nesting rules", "Duplicate ID", "first occurrence of ID", "Unclosed element", "not allowed as child of element", "unclosed elements", "not allowed on element", "unquoted attribute value", "Duplicate attribute"],
@@ -18,6 +20,8 @@ javascript: (function() {
 		result,
 		resultText,
 		results,
+		resultsErrors = {},
+		resultsWarnings = {},
 		root = document.getElementById("results");
 	if (!root) {
 		return;
@@ -25,18 +29,43 @@ javascript: (function() {
 	results = root.getElementsByTagName("li");
 	for (i = results.length - 1; i >= 0; i--) {
 		result = results[i];
+
+		// Filter to only match first-level list items
 		if (result.id.substr(0, 3) === "vnu") {
 			if (result.className !== "info") {
 				nErr = nErr + 1;
 			}
-			resultText = result.textContent + "";
+
+			resultText = "" + result.textContent; // Result to string
+			resultText = resultText.substring(0, resultText.indexOf('.')); // Trim
+
 			if (resultText.match(filterRE) == null) {
 				result.style.display = "none";
 				result.className = result.className + "steveNoLike";
 			} else {
 				nAcc = nAcc + 1;
+
+				if (resultText.substring(0, 6) == 'Error:') {
+					resultsErrors[resultText.substr(7)] = true;
+				}
+			}
+			if (resultText.substring(0, 8) == 'Warning:') {
+				resultsWarnings[resultText.substr(9)] = true;
 			}
 		}
 	}
-	alert(nErr + " errors and warnings, " + nAcc + " related to accessibility");
+	resultText = "";
+	for (i in resultsWarnings) {
+		if (resultsWarnings.hasOwnProperty(i)) {
+			resultText = i + "; " + resultText;
+		}
+	}
+	resultText = "\n\nWarnings: \n" + resultText;
+	for (i in resultsErrors) {
+		if (resultsErrors.hasOwnProperty(i)) {
+			resultText = i + "; " + resultText;
+		}
+	}
+	resultText = nErr + " errors and warnings.\n\n" + nAcc + " errors found that impact accessibility: " + resultText;
+	alert(resultText);
 })();
