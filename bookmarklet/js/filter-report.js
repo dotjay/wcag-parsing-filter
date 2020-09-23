eea9865e1a63107bbad199b2f06ecd553b74f374
Jon Gibbins <hello@dotjay.com>
2020-09-23 17:15:40 +0100

    Allow grouped error reports. Smaller bookmarklet code.

--- bookmarklet/js/filter-report.js
+++ bookmarklet/js/filter-report.js
@@ -10,16 +10,15 @@
  * Bookmarklet
  * Compressed with Crunchinator:
  * http://ted.mielczarek.org/code/mozilla/bookmarklet.html
- * javascript:(function(){javascript:(function(){var filters=["tag seen","Stray end tag","Bad start tag","violates nesting rules","Duplicate ID","Unclosed element","not allowed as child of element","unclosed elements","unquoted attribute value","Duplicate attribute","descendant of an element with the attribute"].join("|"),i,nT=0,nP1=0,result,resultText,results,resultsP1={},root=document.getElementById("results");if(!root){return;}results=root.getElementsByTagName("li");for(i=results.length-1;i>=0;i--){result=results[i];if(result.id.substr(0,3)==="vnu"){if(result.className!=="info"){nT=nT+1;}resultText=""+result.textContent;resultText=resultText.substring(0,resultText.indexOf('.'));if(resultText.match(filters)==null){result.style.display="none";result.className=result.className+"a11y-ignore";}else{resultsP1[resultText.substr(7)]=true;nP1=nP1+1;}}}resultText="";for(i in resultsP1){if(resultsP1.hasOwnProperty(i)){resultText=resultText+"- "+i+"\n";}}alert(nT+" validation errors and warnings.\n"+nP1+" instances of errors known to create accessibility issues:\n"+resultText);})();})();
+ * javascript:(function(){javascript:(function(){var filters=["tag seen","Stray end tag","Bad start tag","violates nesting rules","Duplicate ID","Unclosed element","not allowed as child of element","unclosed elements","unquoted attribute value","Duplicate attribute","descendant of an element with the attribute"].join("|"),groups={"Duplicate ID":true},e,g,i,m,nT=0,nP1=0,r,rt,res=document.getElementById("results"),rP1={};if(!res){return;}res=res.getElementsByTagName("li");for(i=res.length-1;i>=0;i--){r=res[i];if(r.id.substr(0,3)==="vnu"){if(r.className!=="info"){nT=nT+1;}rt=""+r.textContent;rt=rt.substring(0,rt.indexOf('.'));m=rt.match(filters);if(m==null){r.style.display="none";r.className=r.className+"a11y-ignore";}else{e=rt.substr(7);g=m[0];if(groups.hasOwnProperty(g)){rP1[g]=rP1[g]||{};e=e.substr(g.length+1);rP1[g][e]=true;}else{rP1[e]=true;}nP1=nP1+1;}}}rt="";for(i in rP1){if(rP1.hasOwnProperty(i)){e=rP1[i];if(e===true){rt=rt+"- "+i+"\n";}else{rt=rt+"- "+i+"\n";for(m in e){if(e.hasOwnProperty(m)){rt=rt+"  + "+m+"\n";}}}}}alert(nT+" validation errors and warnings.\n"+nP1+" instances of errors known to create accessibility issues:\n"+rt);})();})();
  */
 javascript: (function () {
 	var filters = [
-				{"Duplicate ID"},
 				"tag seen",
 				"Stray end tag",
 				"Bad start tag",
 				"violates nesting rules",
-				"Duplicate ID"
+				"Duplicate ID",
 				// "first occurrence of ID", // Warning related to "Duplicate ID"
 				"Unclosed element",
 				"not allowed as child of element",
@@ -29,84 +28,112 @@ javascript: (function () {
 				"Duplicate attribute",
 				"descendant of an element with the attribute"
 			].join("|"),
-		i,
+	  groups = {"Duplicate ID": true},
+		e, // Error
+		g, // Group
+		i, // Iterator
+		m, // Match
 		nT = 0, // Total validation errors and warnings
 		nP1 = 0, // Errors affecting accessibility at P1
 		// nP2 = 0, // Errors affecting accessibility at P2
-		result,
-		resultText,
-		results,
-		resultsP1 = {},
-		// resultsP2 = {},
+		r, // Result
+		rt, // Result text / Report
+		res = document.getElementById("results"),
+		rP1 = {}; // Priority 1 results
+		// rP2 = {}, // Priority 2 results
 		// resultsWarnings = {},
-		root = document.getElementById("results");
 
-	if (!root) {
+	if (!res) {
 		return;
 	}
 
-	results = root.getElementsByTagName("li");
+	res = res.getElementsByTagName("li");
 
-	for (i = results.length - 1; i >= 0; i--) {
-		result = results[i];
+	for (i = res.length - 1; i >= 0; i--) {
+		r = res[i];
 
 		// Filter to only match first-level list items
-		if (result.id.substr(0, 3) === "vnu") {
-			if (result.className !== "info") {
+		if (r.id.substr(0, 3) === "vnu") {
+			if (r.className !== "info") {
 				nT = nT + 1;
 			}
 
-			resultText = "" + result.textContent; // Result to string
-			resultText = resultText.substring(0, resultText.indexOf('.')); // Trim
+			rt = "" + r.textContent; // Result to string
+			rt = rt.substring(0, rt.indexOf('.')); // Trim
 
-			if (resultText.match(filters) == null) {
-				result.style.display = "none";
-				result.className = result.className + "a11y-ignore";
+			m = rt.match(filters);
+
+			if (m == null) {
+				r.style.display = "none";
+				r.className = r.className + "a11y-ignore";
 			}
-			// else if (resultText.match("not allowed on element") != null) {
+			// else if (rt.match("not allowed on element") != null) {
 			// 	// Separate "Attribute X not allowed on element" errors from others
-			// 	resultsP2[resultText.substr(7)] = true;
+			// 	rP2[rt.substr(7)] = true;
 			// 	nP2 = nP2 + 1;
 			// }
 			else {
-				// All other errors
-				resultsP1[resultText.substr(7)] = true;
+				e = rt.substr(7);
+				g = m[0];
+
+				if (groups.hasOwnProperty(g)) {
+					// Handle grouped errors
+					rP1[g] = rP1[g] || {};
+					e = e.substr(g.length + 1);
+					rP1[g][e] = true;
+				}
+				else {
+					// All other errors
+					rP1[e] = true;
+				}
 				nP1 = nP1 + 1;
 			}
 
-			//if (resultText.substring(0, 8) == 'Warning:') {
-			//	resultsWarnings[resultText.substr(9)] = true;
+			//if (rt.substring(0, 8) == 'Warning:') {
+			//	resultsWarnings[rt.substr(9)] = true;
 			//}
 		}
 	}
 
-	resultText = "";
+	rt = "";
 
 	// Add warnings to report
 	//for (i in resultsWarnings) {
 	//	if (resultsWarnings.hasOwnProperty(i)) {
-	//		resultText = i + "; " + resultText;
+	//		rt = i + "; " + rt;
 	//	}
 	//}
-	//resultText = "\nWarnings:\n" + resultText;
+	//rt = "\nWarnings:\n" + rt;
 
 	// Add P2 errors to report
-	// for (i in resultsP2) {
-	// 	if (resultsP2.hasOwnProperty(i)) {
-	// 		resultText = i + "; " + resultText;
+	// for (i in rP2) {
+	// 	if (rP2.hasOwnProperty(i)) {
+	// 		rt = i + "; " + rt;
 	// 	}
 	// }
-	// resultText = "\n" + nP2 + " low priority errors:\n" + resultText + "\n";
+	// rt = "\n" + nP2 + " low priority errors:\n" + rt + "\n";
 
 	// Add P1 errors to report
-	for (i in resultsP1) {
-		if (resultsP1.hasOwnProperty(i)) {
-			resultText = resultText + "- " + i + "\n";
+	for (i in rP1) {
+		if (rP1.hasOwnProperty(i)) {
+			e = rP1[i];
+			if (e === true) {
+				rt = rt + "- " + i + "\n";
+			}
+			else {
+				// Handle grouped errors
+				rt = rt + "- " + i + "\n";
+				for (m in e) {
+					if (e.hasOwnProperty(m)) {
+						rt = rt + "  + " + m + "\n";
+					}
+				}
+			}
 		}
 	}
-	// resultText = "\n" + nP1 + " high priority errors:\n" + resultText + "\n";
+	// rt = "\n" + nP1 + " high priority errors:\n" + rt + "\n";
 
 	// Output report
-	// alert(nT + " validation errors and warnings.\nErrors that may impact accessibility:" + resultText);
-	alert(nT + " validation errors and warnings.\n" + nP1 + " instances of errors known to create accessibility issues:\n" + resultText);
+	// alert(nT + " validation errors and warnings.\nErrors that may impact accessibility:" + rt);
+	alert(nT + " validation errors and warnings.\n" + nP1 + " instances of errors known to create accessibility issues:\n" + rt);
 })();
